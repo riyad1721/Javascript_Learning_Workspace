0f08997716bb2d3ca3590e352f540181498e89ba
riyad1721 <reyadhasan7254@gmail.com>
2022-03-18 23:40:03 +0600

    more practice

---
+++ Fake-Data/fake.js
@@ -0,0 +1,51 @@
+const person = [
+    {
+        id: 1,
+        name: 'Eren Li',
+        price: 5,
+        favorite: 'Nothing',
+        image: 'https://images.pexels.com/photos/7188928/pexels-photo-7188928.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+
+    },
+    {
+        id: 2,
+        name: 'kindel Media',
+        price: 5,
+        favorite: 'Traveling',
+        image: 'https://images.pexels.com/photos/8301492/pexels-photo-8301492.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+
+    },
+    {
+        id: 3,
+        name: 'Lukas Rychvalsky',
+        price: 7,
+        favorite: 'Walking',
+        image: 'https://images.pexels.com/photos/670786/pexels-photo-670786.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+
+    },
+    {
+        id: 4,
+        name: 'Pavel Danilyuk',
+        price: 10,
+        favorite: 'Driving',
+        image: 'https://images.pexels.com/photos/7405796/pexels-photo-7405796.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+
+    },
+    {
+        id: 5,
+        name: 'Anima',
+        price: 9,
+        favorite: 'Masty',
+        image: 'https://images.pexels.com/photos/8709703/pexels-photo-8709703.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+
+    },
+    {
+        id: 6,
+        name: 'Teddy Joseph',
+        price: 12,
+        favorite: 'Business Man',
+        image: 'https://images.pexels.com/photos/2955376/pexels-photo-2955376.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+
+    },
+
+]
